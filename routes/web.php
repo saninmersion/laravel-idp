1934645c85ef600c1ac3cfd0501aa6ea20d5f113
Sandip Shrestha <sandipshrestha@SAMARITAN.local>
2023-02-06 08:53:00 +0545

    added clients

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,6 +24,8 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
+Route::get('/clients', [ClientController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
